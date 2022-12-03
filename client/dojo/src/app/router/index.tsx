5be173ed749fd2a0df9ab80960a25320f29c2033
Alexey <44631357+agmitron@users.noreply.github.com>
2022-12-03 16:08:16 +0800

    chore: hide unnecessary routes

--- client/dojo/src/app/router/index.tsx
+++ client/dojo/src/app/router/index.tsx
@@ -2,44 +2,26 @@ import { createBrowserRouter } from "react-router-dom";
 import LoginPage from "~/pages/auth/login/ui";
 import RegisterPage from "~/pages/auth/register/ui";
 import Dojo from "~/pages/dojo/ui";
-import Editor from "~/pages/editor";
-import Library from "~/pages/library";
 import Layout from "../layout/ui";
 
 const router = createBrowserRouter(
   [
     {
-      path: "dojo/:compositionId",
+      path: "composition/:compositionId",
       element: (
         <Layout title="Dojo">
           <Dojo />
         </Layout>
       ),
     },
-    {
-      path: "library",
-      element: (
-        <Layout title="Library">
-          <Library />
-        </Layout>
-      ),
-    },
-    {
-      path: "editor/:compositionId",
-      element: (
-        <Layout title="Editor">
-          <Editor />
-        </Layout>
-      ),
-    },
-    {
-      path: "auth/login",
-      element: <LoginPage />,
-    },
-    {
-      path: "auth/register",
-      element: <RegisterPage />,
-    },
+    // {
+    //   path: "auth/login",
+    //   element: <LoginPage />,
+    // },
+    // {
+    //   path: "auth/register",
+    //   element: <RegisterPage />,
+    // },
   ]
 );
 
